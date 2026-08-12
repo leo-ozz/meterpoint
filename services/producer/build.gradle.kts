57bbfcbc7b70aa75d82ffa730b3f7e45d703bff1
leo-ozz <leofrommer1@googlemail.com>
2026-08-12 09:34:51 +0200

    Added missing Spring Boot Flyway dependency

--- services/producer/build.gradle.kts
+++ services/producer/build.gradle.kts
@@ -21,6 +21,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.springframework.boot:spring-boot-starter-webmvc")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-flyway")
 	implementation("org.flywaydb:flyway-core")
 	implementation("org.flywaydb:flyway-database-postgresql")
 	
